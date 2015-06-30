bb529277f387df7af4a5235e953bc0e64088cccd
Sqrt of Saturn <sqrt@octoblu.com>
2015-06-29 23:00:10 -0700

    fixes

--- meshblu_bale_notify.js
+++ meshblu_bale_notify.js
@@ -3,6 +3,7 @@ var meshblu = require('meshblu');
 var config = require('./meshblu.json');
 
 var baleCount;
+var prev;
 
 
 var conn = meshblu.createConnection({
@@ -12,6 +13,24 @@ var conn = meshblu.createConnection({
   token  : config.token
 });
 
+conn.on('ready', function(){
+
+  console.log("ALIVE!");
+
+  setInterval(function() {
+
+    if(prev != baleCount){
+      conn.message({
+        "devices": "*",
+        "payload": {"baleCount" : baleCount}
+      });
+      prev = baleCount;
+    }
+
+  }, 1000);
+
+}); // end meshblu On ready
+
 
 // only scan for devices advertising these service UUID's (default or empty array => any peripherals
 var serviceUuids = ['b1a6752152eb4d36e13e357d7c225466'];
@@ -20,56 +39,38 @@ var serviceUuids = ['b1a6752152eb4d36e13e357d7c225466'];
 var allowDuplicates = false;
 
 
-conn.on('ready', function(){
 
-  console.log("ALIVE!");
-
-  setInterval(function() {
-
-    conn.message({
-      "devices": "*",
-      "payload": {"baleCount" : baleCount}
-    });
-
-  }, 5000);
+noble.on('stateChange', function(state) {
+  if (state === 'poweredOn') {
+    noble.startScanning(['b1a6752152eb4d36e13e357d7c225466'], false);
+  } else {
+    noble.stopScanning();
+  }
+});
 
 
+noble.on('discover', function(peripheral) {
+  peripheral.connect(function(error) {
+    console.log('connected to peripheral: ' + peripheral.uuid);
+    peripheral.discoverServices(['b1a6752152eb4d36e13e357d7c225466'], function(error, services) {
+      //console.log("SERVICES:", services);
+      var baleService = services[0];
+      console.log('discovered Bale service');
 
+      baleService.discoverCharacteristics(['4b842c60ddd611e38b680800200c9a66'], function(error, characteristics) {
+        var baleLevelCharacteristic = characteristics[0];
+        console.log('discovered Bale count characteristic');
 
-}); // end meshblu On ready
+        baleLevelCharacteristic.on('read', function(data, isNotification) {
+          console.log('bale count is: ', data.toString() );
+          baleCount = data.toString();
+        });
 
-
-  noble.on('stateChange', function(state) {
-    if (state === 'poweredOn') {
-      noble.startScanning(['b1a6752152eb4d36e13e357d7c225466'], false);
-    } else {
-      noble.stopScanning();
-    }
-  });
-
-
-  noble.on('discover', function(peripheral) {
-    peripheral.connect(function(error) {
-      console.log('connected to peripheral: ' + peripheral.uuid);
-      peripheral.discoverServices(['b1a6752152eb4d36e13e357d7c225466'], function(error, services) {
-        //console.log("SERVICES:", services);
-        var baleService = services[0];
-        console.log('discovered Bale service');
-
-        baleService.discoverCharacteristics(['4b842c60ddd611e38b680800200c9a66'], function(error, characteristics) {
-          var baleLevelCharacteristic = characteristics[0];
-          console.log('discovered Bale count characteristic');
-
-          baleLevelCharacteristic.on('read', function(data, isNotification) {
-            console.log('bale count is: ', data.toString() );
-            baleCount = data.toString();
-          });
-
-          // true to enable notify
-          baleLevelCharacteristic.notify(true, function(error) {
-            console.log('bale level notification on');
-          });
+        // true to enable notify
+        baleLevelCharacteristic.notify(true, function(error) {
+          console.log('bale level notification on');
         });
       });
     });
   });
+});
